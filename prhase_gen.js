e211f163595886d2859eda72bf48467eb6d411fc
unknown <hebertsa07@gmail.com>
2018-01-24 16:13:05 -0500

    minor edit

--- prhase_gen.js
+++ prhase_gen.js
@@ -2,14 +2,13 @@ document.addEventListener('DOMContentLoaded',(event)=>{
     
     let words = []
     document.getElementById('file').addEventListener('change', readFile, false)
+    
     document.getElementById('gen').addEventListener('click', function(event){downloadCSV(words)}, false)
 
 
     function readFile(event) {
         let files = event.target.files
-        // console.log(files)
         let file = files[0]
-        // console.log(file)
         let reader = new FileReader()
         reader.onload = function(event){
             console.log("success")
@@ -21,7 +20,6 @@ document.addEventListener('DOMContentLoaded',(event)=>{
 
     function dataToArray(data){
         let newArray = data.split(',')
-        // console.log(newArray)
         return newArray
     }
 
